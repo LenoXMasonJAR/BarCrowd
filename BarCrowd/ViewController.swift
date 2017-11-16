8712037552fe1bbac6f069b4b0b7f5f4b188c42a
LenoXMasonJAR <mahonema5@gmail.com>
2017-11-16 14:35:53 -0500

    BarCrowd initial push to GitHub

--- BarCrowd/ViewController.swift
+++ BarCrowd/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // dismiss keyboard when tapping outside oftext fields
+        let detectTouch = UITapGestureRecognizer(target: self, action:
+            #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
+        
+        // make this controller the delegate of the text fields.
+        self.emailField.delegate = self
+        self.passwordField.delegate = self
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func validateFields() -> Bool {
+        let pwOk = self.isValidPassword(password: self.passwordField.text)
+        if !pwOk {
+            print("Invalid password")
+        }
+        
+        let emailOk = self.isValidEmail(emailStr: self.emailField.text)
+        if !emailOk {
+            print("Invalid email address")
+        }
+        
+        return emailOk && pwOk
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: Any) {
+    }
+    
+}
 
-
+extension ViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.emailField {
+            self.passwordField.becomeFirstResponder()
+        } else {
+            if self.validateFields() {
+                print(NSLocalizedString("Congratulations! You entered correct values.", comment: ""))
+            }
+        }
+        return true
+    }
 }
 
